00ec6f934d3f0b83b3d5fc4e6d34d2c272f210ef
BODY1998 <aymanabdo815@gmail.com>
2023-08-01 07:51:28 +0300

    weather.html page finished

--- weather_api/views.py
+++ weather_api/views.py
@@ -4,7 +4,7 @@ import requests
 # Create your views here.
 
 
-def index(request):
+def weather(request):
     api_url = 'http://api.openweathermap.org./data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
     city = 'cairo'
     url = api_url + city
@@ -12,11 +12,11 @@ def index(request):
     respone = requests.get(url)
     object_content = respone.json()
     city_info = {
-        'name': object_content.name,
+        'name': object_content['name'],
         'country': object_content['sys']['country'],
         'temperature': object_content['main']['temp'],
-        'description': object_content['weather']['0']['description'],
-        'icon': object_content['weather']['0']['icon'],
+        'description': object_content['weather'][0]['description'],
+        'icon': object_content['weather'][0]['icon'],
     }
-    
-    return render(request,'weather.html',{'city_info':city_info})
+
+    return render(request, 'weather_api/weather.html', city_info)
